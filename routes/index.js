0d1169a8bb8fa94c437c9dcc9e9aef1a921fcdcc
Andy Tuttle <andytuttle12@gmail.com>
2017-02-08 14:17:28 -0500

    Added basic SQL integration with buttons and shared images.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/config');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : config.host,
+  user     : config.user,
+  password : config.password,
+  database : config.database
+});
+
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Rate the Cars' });
+	var getImagesQuery = "SELECT * FROM images";
+	connection.query(getImagesQuery, (error, results, fields)=>{
+		var randomIndex = Math.floor(Math.random() * results.length);
+		var imageUrl = results[randomIndex].imageURL;
+		var imageID = results[randomIndex].id;
+		// res.json(results);
+		res.render('index', { title: 'Rate the Cars', image: imageUrl, imageID: imageID});
+	});
 });
 
 router.get('/standings', function(req, res, next) {
-  res.render('standings', { title: 'Standings' });
+	res.render('standings', { title: 'Standings' });
+});
+
+router.get('/vote/:voteDirection/:imageID', (req, res, next)=>{
+	var imageId = req.params.imageID;
+	var voteDirection = req.params.voteDirection;
+	var insertVoteQuery = "INSERT INTO votes (ip, imageID, voteDirection) VALUES ('"+req.ip+"',"+imageId+",'"+voteDirection+"')";
+	res.send(insertVoteQuery);
 });
 
 module.exports = router;
