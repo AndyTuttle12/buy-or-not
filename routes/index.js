2e9358d86920efb436de3ea760abf0fd3f4cfbcb
Andy Tuttle <andytuttle12@gmail.com>
2017-02-08 11:48:10 -0500

    Updated routes with basic pages.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Rate the Cars' });
 });
 
+router.get('/standings', function(req, res, next) {
+  res.render('standings', { title: 'Standings' });
+});
+
 module.exports = router;
