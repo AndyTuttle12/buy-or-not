e7a497ae8a553101694a9a9c3ae27edb8601b911
Andy Tuttle <andytuttle12@gmail.com>
2017-02-09 10:09:59 -0500

    Fixed crash issue, added more queries.

--- routes/index.js
+++ routes/index.js
@@ -13,25 +13,66 @@ connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var getImagesQuery = "SELECT * FROM images";
+	var getImagesQuery = "SELECT * FROM images WHERE id NOT IN"+
+	"(SELECT imageID FROM votes WHERE ip = '"+req.ip+"');";
 	connection.query(getImagesQuery, (error, results, fields)=>{
 		var randomIndex = Math.floor(Math.random() * results.length);
-		var imageUrl = results[randomIndex].imageURL;
-		var imageID = results[randomIndex].id;
+		console.log(results.length);
 		// res.json(results);
-		res.render('index', { title: 'Rate the Cars', image: imageUrl, imageID: imageID});
+		if(results.length === 0){
+			// res.send('nope');
+			res.render('index', { msg: "Thanks for voting! See the results above in the standings!", title: 'Rate the Cars' });
+		}else{
+			res.render('index', {
+				title: 'Rate the Cars',
+				image: results[randomIndex].imageURL,
+				imageID: results[randomIndex].id,
+				imageName: results[randomIndex].imageName
+			});
+		}
 	});
-});
-
-router.get('/standings', function(req, res, next) {
-	res.render('standings', { title: 'Standings' });
 });
 
 router.get('/vote/:voteDirection/:imageID', (req, res, next)=>{
 	var imageId = req.params.imageID;
 	var voteDirection = req.params.voteDirection;
+	if (voteDirection === "up"){
+		voteDirection = 1;
+	}else{
+		voteDirection = -1;
+	}
 	var insertVoteQuery = "INSERT INTO votes (ip, imageID, voteDirection) VALUES ('"+req.ip+"',"+imageId+",'"+voteDirection+"')";
-	res.send(insertVoteQuery);
+	connection.query(insertVoteQuery, (error, results, fields)=>{
+		if (error) throw error;
+		res.redirect('/');
+	});
 });
+
+router.get('/standings', (req, res, next)=>{
+	// var getCarsQuery = "SELECT * FROM votes";
+	// connection.query(getCarsQuery, (error, results, fields)=>{
+
+		res.render('standings', { title: 'Standings' });
+	// });
+});
+
+router.get('/testQ', (req, res, next)=>{
+	// var id1 = 1;
+	// var id2 = 3;
+	// var query = "SELECT * FROM images WHERE id > ? AND < ?";
+	// connection.query(query, [id1, id2], (error, results, fields)=>{
+	// 	res.json(results);
+	// });
+	var imageIdVoted = 14;
+	var votedDirection = 1;
+	var insertQuery = "INSERT INTO votes (ip, imageID, voteDirection) VALUES ('?','?','?')";
+	connection.query(insertQuery, [req.ip, imageIdVoted, votedDirection], (error, results, fields)=>{
+		var query = "SELECT * FROM votes";
+		connection.query(query, (error, results, fields)=>{
+			res.json(results);
+		});
+	});
+});
+
 
 module.exports = router;
